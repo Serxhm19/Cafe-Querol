fbb188597b198033b2087d5af5349120923ae181
Serxhm19 <sergihernandezfp@ibf.cat>
2024-01-03 22:05:15 +0100

    New Style changes

--- views/detalles_pedidos.php
+++ views/detalles_pedidos.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-SsDkMO9SMV3F8C3QvxZjScNB6eScB8peVp/UL6ZI3jFhAxVdfdC4vhm3ZXiU04QE"
         crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="Style/stylehomepage.css">
+    <link rel="stylesheet" href="Style/styledetallespedido.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
